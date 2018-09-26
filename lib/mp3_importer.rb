7c193797b00226b9695d9b537a6a98cc58f22a27
John Werenski <jtwerenski@gmail.com>
2018-09-26 21:02:59 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,6 +10,6 @@ class MP3Importer
   end
 
   def import
-    file_names.each {|filename| Song.new_by_filename(filename)}
+    self.files.each {|filename| Song.new_by_filename(filename)}
   end
 end
